c0837876ee070bb3c6db9679651077b9bb1e5bb1
VanChien <phamvanbk11@gmail.com>
2016-08-25 23:13:48 +0700

    73903 export pdf word list

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,5 +4,15 @@ class WordsController < ApplicationController
     @words = Word.of_category(params[:category_id])
       .result_filter(current_user.id, params[:check_learned])
       .paginate(page: params[:page], per_page: Settings.per_page)
+    respond_to do |format|
+      format.html
+      format.xlsx {render xlsx: "export_excel", filename: "words.xlsx"}
+      format.pdf do
+        pdf = WickedPdf.new
+          .pdf_from_string(render_to_string("words/export_pdf.html.erb",
+          layout: false), orientation: "Landscape")
+        send_data(pdf, filename: "words.pdf")
+      end
+    end
   end
 end
