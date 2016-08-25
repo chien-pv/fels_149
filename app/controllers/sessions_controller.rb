38da3ec0e429eb7b77244b8097bda3f36204c9a1
trantrinh1102 <trantrinh.1102@gmail.com>
2016-08-25 10:42:17 +0700

    Show user home

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       login user
       params[:remember] == "1" ? remember(user) : forget(user)
-      redirect_to root_path
+      redirect_to user_path(user)
     else
       flash.alert = t "sessions.login_fail"
       redirect_to new_session_path
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @user = current_user
+    end
   end
 end
