74d7ff02691f9784e9fb973cbdbe0401f0155119
trantrinh1102 <trantrinh.1102@gmail.com>
2016-08-31 14:04:45 +0700

    [Admin] delete and update word and answer

--- app/assets/javascripts/admin/words.coffee
+++ app/assets/javascripts/admin/words.coffee
@@ -4,7 +4,8 @@ ready = ->
   clearChoice = ->
     i = 0
     while i < answers.length
-      $(answers[i]).prop 'checked', false
+      $("#correct_answer_"+ i).prop 'checked', false
+      $(".correct_answer_"+ i).val 'false'
       i++
     return
 
@@ -13,8 +14,27 @@ ready = ->
     $(answers[i]).on 'click', ->
       clearChoice()
       $(this).prop 'checked', true
+      $('.'+$(this).attr('id')).val 'true'
       return
     i++
   return
+
+
 $(document).ready(ready)
 $(document).on('page:load', ready)
+
+$(document).ready ->
+  $('body').on 'click', '.btn-delete-word', ->
+      $word = $(this).closest('tr')
+      if confirm('Are you sure ?')
+        $.ajax
+          type: 'DELETE'
+          url: '/admin/words/' + $(this).data('value')
+          dataType: 'json'
+          success: (data) ->
+            $word.remove()
+            $('.flash-massage').replaceWith('<div class="alert alert-success">'+data.flash[0][1]+'</div>')
+            return
+          error: (error_message) ->
+            alert 'error' + error_message
+            return
--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,5 +1,6 @@
 class Admin::WordsController < ApplicationController
   before_action :authenticate!, :authenticate_admin!
+  before_action :load_word, only: [:destroy, :edit, :update]
 
   def index
     @categories = Category.all
@@ -17,15 +18,40 @@ class Admin::WordsController < ApplicationController
     if @category
       @word = @category.words.new word_params
       if @word.save
-        flash[:success] = "success"
+        flash[:success] = t "admin.words.flash.success"
         redirect_to admin_words_path
       else
         4.times{@word.answers.build}
         render :new
       end
     else
-      flash[:danger] = t "admin.words.category"
+      flash[:danger] = t "admin.words.flash.category"
       redirect_to new_admin_word_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @word.update(word_params)
+      flash[:success] = t "admin.words.flash.update"
+      redirect_to admin_words_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @word.destroy
+      flash.now[:success] = t "admin.words.flash.delete"
+    end
+    respond_to do |format|
+      format.json do
+        render json:{
+          flash: flash
+        }
+      end
     end
   end
 
@@ -34,5 +60,9 @@ class Admin::WordsController < ApplicationController
   def word_params
     params.require(:word).permit(:content,
       answers_attributes: [:id, :content, :is_correct])
+  end
+
+  def load_word
+    @word = Word.find_by id: params[:id]
   end
 end
