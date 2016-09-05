127485e56e98bc2100b4d1956260379718b9e1af
phamvanbk11 <phamvanbk11@gmail.com>
2016-09-05 16:41:02 +0700

    fix bug create lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,11 +7,16 @@ class LessonsController < ApplicationController
 
   def create
     category = Category.find_by id: params[:category_id]
-    lesson = category.lessons.create user: current_user
-    if lesson
-      redirect_to lesson_path lesson
+    if category.words.present?
+      lesson = category.lessons.new user: current_user
+      if lesson.save
+        redirect_to lesson_path lesson
+      else
+        flash[:danger] = t "lessons.create.danger"
+        redirect_to categories_path
+      end
     else
-      flash[:danger] = t "lessons.create.danger"
+      flash[:danger] = t "categories.words.blank"
       redirect_to categories_path
     end
   end
